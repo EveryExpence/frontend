f5f13c8410efdee09db890092ead3d8b184449e3
justkinou <justkinou@proton.me>
2026-04-10 01:52:55 +0200

    add tailwind support

--- app/(auth)/login/index.tsx
+++ app/(auth)/login/index.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, TextInputChangeEvent } from 'react-native'
+import React, { useState } from 'react'
 
 const index = () => {
-  return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  )
+    const styles = StyleSheet.create({
+        container: {
+            paddingHorizontal: 20,
+        }
+    });
+
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    return (
+        <View className="flex-1 items-center justify-center" style={styles.container}>
+            <TextInput
+                placeholder='Enter email'
+                textContentType="emailAddress"
+                value={email}
+                onChangeText={setEmail}
+            />
+
+            <TextInput
+                placeholder='Enter password'
+                textContentType="password"
+                value={password}
+                onChangeText={setPassword}
+            />
+        </View>
+    )
 }
 
 export default index
