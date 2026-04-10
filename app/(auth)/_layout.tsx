205266c8cc7c5164bb4ec3d48fc939d34491b2f3
justkinou <justkinou@proton.me>
2026-04-10 02:57:01 +0200

    add app branding element

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -5,8 +5,28 @@ import { Slot } from 'expo-router'
 const _layout = () => {
   return (
     <View className="flex-1">
-        
-        
+        <View
+            className="absolute rounded-full"
+            style={{
+                backgroundColor: "#4370C7",
+                width: 600,
+                height: 600,
+                top: -350,
+                left: -20,
+            }}
+        />
+
+        <View
+            className="absolute gap-5"
+            style={{
+                top: 50,
+                right: 10,
+            }}
+        >
+            <Text className="text-6xl font-bold text-white text-right">EveryExpense</Text>
+            <Text className="text-2xl text-white text-right">Be aware of your expenses</Text>
+        </View>
+
         <Slot />
     </View>
   )
