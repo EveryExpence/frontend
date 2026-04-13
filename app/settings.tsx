c0937297207bc71569c82e8a3a3e0f36ed306771
Dmytro Malinovskyi <malynovskydmitriyy@gmail.com>
2026-04-13 21:31:20 +0200

    add react-icons, replace items in SectionList with object to handle icons

--- app/settings.tsx
+++ app/settings.tsx
@@ -5,22 +5,32 @@ import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 const DATA = [
     {
         title: "Account",
-        data: ['Name'],
+        data: [
+            {title: 'Name', icon: ''},
+        ],
     },
     
     {
         title: "Preferences",
-        data: ['Language', 'Theme']
+        data: [
+            {title: 'Language', icon: ''},
+            {title: 'Theme', icon: ''},
+        ]
     },
 
     {
         title: "Notifications",
-        data: ['Push Notifications']
+        data: [
+            {title: 'Push Notifications', icon: ''},
+        ]
     },
 
     {
         title: "Security",
-        data: ['Change Password', 'Log Out']
+        data: [
+            {title: 'Change Password', icon: ''}, 
+            {title: 'Log Out', icon: ''},
+        ]
     },
 ]
 
@@ -30,18 +40,22 @@ const SettingsScreen = () => {
         <SafeAreaView style={{ flex: 1, backgroundColor: '#ffffff' }} edges={['top']}>
             <SectionList
                 sections={DATA}
-                keyExtractor={(item, index) => item + index}
+                keyExtractor={(item, index) => item.title + index}
                 renderSectionHeader = { ({section: {title}}) => ( 
-                    <Text className= "text-3xl pl-2 text-theme-text text-black font-semibold">{ title }</Text> 
+                    <Text className= "text-3xl text-black font-semibold pl-4">{ title }</Text> 
                 )}
                 renderItem = { ({item}) => (
-                    <View style={{height: 100, backgroundColor: '#c9cccf'}}>
-                        <Text className= "text-2xl pl-2 text-theme-text text-black py-7 font-semibold">{ item }</Text>
+                    <View style={{height: 100, backgroundColor: '#c9cccf', marginHorizontal: 16, borderRadius: 5}}>
+                        <Text className= "text-2xl pl-2 text-black py-7 font-semibold">{ item.title }</Text>
                     </View>
                 )}
 
                 ItemSeparatorComponent = {() => (
-                    <View style={{ height: 1, backgroundColor: '#333', marginHorizontal: 10 }} />
+                    <View style={{ height: 1, backgroundColor: '#333', marginHorizontal: 26 }} />
+                )}
+
+                SectionSeparatorComponent = {() => (
+                    <View style={{ height: 15, backgroundColor: '#ffffff' }} />
                 )}
             />
         </SafeAreaView>
