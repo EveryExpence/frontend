90fd701b8ab1cedb833180a1f54d1c98da279d65
justkinou <justkinou@proton.me>
2026-04-27 19:44:08 +0200

    add payment method tests

--- tests/data.test.ts
+++ tests/data.test.ts
@@ -1,5 +1,6 @@
 import { createCategory, deleteCategory, getAllCategories, updateCategory } from "@/data/categories";
 import { migrateDatabase } from "@/data/init";
+import { createPaymentMethod, deletePaymentMethod, getAllPaymentMethods, updatePaymentMethod } from "@/data/paymentMethods";
 import { Category } from "@/types/data/category";
 import { openDatabaseAsync, SQLiteDatabase } from "expo-sqlite";
 
@@ -38,26 +39,64 @@ describe("data module", () => {
 
     test("category should be updated", async () => {
         let categories = await getAllCategories(db);
-        const category = categories.find((category: Category) => category.name === "Transport");
+        const category = categories.find((category) => category.name === "Transport");
         expect(category).not.toBeUndefined();
         await updateCategory(db, category!.id, { name: "Gifts", type: "income" });
         
         categories = await getAllCategories(db);
-        const editedCategory = categories.find((category: Category) => category.name === "Gifts");
-        expect(editedCategory).not.toBeUndefined();
-        expect(editedCategory?.type).toEqual("income");
+        const edited = categories.find((category) => category.name === "Gifts");
+        expect(edited).not.toBeUndefined();
+        expect(edited?.type).toEqual("income");
     });
 
     test("category should be deleted", async () => {
         let categories = await getAllCategories(db);
-        const id = categories.find((category: Category) => category.name === "Gifts")!.id;
+        const id = categories.find((category) => category.name === "Gifts")!.id;
         await deleteCategory(db, id);
 
         categories = await getAllCategories(db);
         expect(categories.length).toBe(2);
 
-        const names = categories.map((category: Category) => category.name);
+        const names = categories.map((category) => category.name);
         expect(names).toContain("Job");
         expect(names).toContain("Food");
+    });
+
+    test("payment methods should be added", async () => {
+        await createPaymentMethod(db, { name: "Card" });
+        await createPaymentMethod(db, { name: "Cash" });
+        await createPaymentMethod(db, { name: "BLIK" });
+
+        const paymentMethods = await getAllPaymentMethods(db);
+        expect(paymentMethods.length).toBe(3);
+
+        const names = paymentMethods.map((paymentMethod) => paymentMethod.name);
+        expect(names).toContain("Card");
+        expect(names).toContain("Cash");
+        expect(names).toContain("BLIK");
+    });
+
+    test("payment method should be updated", async () => {
+        let paymentMethods = await getAllPaymentMethods(db);
+        const paymentMethod = paymentMethods.find((paymentMethod) => paymentMethod.name === "Card");
+        expect(paymentMethod).not.toBeUndefined();
+        await updatePaymentMethod(db, paymentMethod!.id, { name: "Credit Card" });
+        
+        paymentMethods = await getAllPaymentMethods(db);
+        const edited = paymentMethods.find((paymentMethod) => paymentMethod.name === "Credit Card");
+        expect(edited).not.toBeUndefined();
+    });
+
+    test("payment method should be deleted", async () => {
+        let paymentMethods = await getAllPaymentMethods(db);
+        const id = paymentMethods.find((paymentMethod) => paymentMethod.name === "Credit Card")!.id;
+        await deletePaymentMethod(db, id);
+
+        paymentMethods = await getAllPaymentMethods(db);
+        expect(paymentMethods.length).toBe(2);
+
+        const names = paymentMethods.map((paymentMethod) => paymentMethod.name);
+        expect(names).toContain("Cash");
+        expect(names).toContain("BLIK");
     });
 })
