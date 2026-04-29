f6fe0f5c9f5fdc12bf636f5d68284f6cff539039
Mikita Dzeviatau <9mikita@gmail.com>
2026-04-30 01:44:53 +0200

    remove SafeArea from settings and add id to the layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,76 @@
-import { View } from 'react-native'
-import React from 'react'
-import { Slot } from 'expo-router'
-import './global.css'
-import { AuthProvider } from '@/context/authContext'
-import CustomizedToast from '@/components/Toast'
-import { SQLiteProvider } from 'expo-sqlite';
-import { migrateDatabase } from '@/data/init'
-import 'react-native-get-random-values';
+import { ActivityIndicator, View } from "react-native";
+import React, { useEffect } from "react";
+import {
+  Slot,
+  useRootNavigationState,
+  useRouter,
+  useSegments,
+} from "expo-router";
+import "./global.css";
+import { AuthProvider, useAuth } from "@/context/authContext";
+import CustomizedToast from "@/components/Toast";
+import { SQLiteProvider } from "expo-sqlite";
+import { migrateDatabase } from "@/data/init";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-const RootLayout = () => {
+const RooLayout = () => {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={{
+          flex: 1,
+        }}
+        className="bg-theme-background"
+      >
+        <AuthProvider>
+          <SQLiteProvider databaseName="app.db" onInit={migrateDatabase}>
+            <Helper />
+          </SQLiteProvider>
+        </AuthProvider>
+        <CustomizedToast />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const Helper = () => {
+  const { user, isInitializing } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+  const navigationState = useRootNavigationState();
+  const inAuthGroup = segments[0] === "(auth)";
+
+  useEffect(() => {
+    if (isInitializing) {
+      return;
+    }
+
+    if (!navigationState?.key) {
+      return;
+    }
+
+    if (user === null && !inAuthGroup) {
+      setTimeout(() => {
+        router.replace("/login");
+      }, 0);
+    }
+
+    if (user !== null && inAuthGroup) {
+      setTimeout(() => {
+        router.replace("/");
+      }, 0);
+    }
+  }, [user, inAuthGroup, navigationState, isInitializing, router]);
+
+  if (isInitializing) {
     return (
-        <View
-            style={{
-                flex: 1,
-            }}
-            className="bg-theme-background"
-        >
-            <AuthProvider>
-                <SQLiteProvider 
-                    databaseName="app.db"
-                    onInit={migrateDatabase}
-                >
-                    <Slot />
-                </SQLiteProvider>
-            </AuthProvider>
-            <CustomizedToast />
-        </View>
-    )
-}
+      <View className="flex flex-1 justify-center items-center">
+        <ActivityIndicator size={32} />
+      </View>
+    );
+  }
 
-export default RootLayout
+  return <Slot />;
+};
+
+export default RooLayout;
