0ce3a94bb071b77f5a2b21ce69507b636e89d5c0
lostbtw <253811@edu.p.lodz.pl>
2026-04-30 22:54:57 +0200

    correctly resolve conflicts caused by merging

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import { View } from 'react-native'
 import React from 'react'
 import { Slot } from 'expo-router'
 import './global.css'
-import { AuthProvider, useAuth } from '@/context/authContext'
+import { AuthProvider } from '@/context/authContext'
 import CustomizedToast from '@/components/Toast'
 import { SQLiteProvider } from 'expo-sqlite';
 import { migrateDatabase } from '@/data/init'
@@ -29,48 +29,4 @@ const RootLayout = () => {
     )
 }
 
-<<<<<<< feature/accounts-page-UI
-const Helper = () => {
-    const { user, isInitializing } = useAuth();
-    const segments = useSegments();
-    const router = useRouter();
-    const navigationState = useRootNavigationState();
-    const inAuthGroup = segments[0] === '(auth)';
-
-    useEffect(() => {
-        if (isInitializing) {
-            return;
-        }
-
-        if (!navigationState?.key) {
-            return;
-        }
-
-        if (user === null && !inAuthGroup) {
-            setTimeout(() => {
-                router.replace("/login");
-            }, 0);
-        }
-    
-        if (user !== null && inAuthGroup) {
-            setTimeout(() => {
-                router.replace("/");
-            }, 0);
-        }
-    }, [user, inAuthGroup, navigationState, isInitializing, router])
-
-    if (isInitializing) {
-        return (
-            <View className="flex flex-1 justify-center items-center">
-                <ActivityIndicator size={32} />
-            </View>
-        )
-    }
-
-    return <Slot />;
-}
-
 export default RootLayout
-=======
-export default RooLayout
->>>>>>> dev
