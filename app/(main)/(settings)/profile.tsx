ec930695c80d7cc62ed22078086be3dec8ec348e
Dmytro Malinovskyi <malynovskydmitriyy@gmail.com>
2026-05-03 18:14:01 +0200

    fully complete form handling

--- app/(main)/(settings)/profile.tsx
+++ app/(main)/(settings)/profile.tsx
@@ -1,27 +1,36 @@
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {Controller, useForm} from 'react-hook-form';
 import { useThemeColor } from "@/hooks/use-theme-color";
 
 const ProfileScreen = () => {
-  const iconColor = useThemeColor({}, "icon");
+  const { control, watch, handleSubmit, formState: {errors} } = useForm({
+    defaultValues:{
+      firstName: "John",
+      lastName: "Doe",
+      email: "john@email.com",
+    }
+  });
 
+  const iconColor = useThemeColor({}, "icon");
   const [isEditing, setIsEditing] = useState(false)
 
-  const [form, setForm] = useState({
-    firstName: "John",
-    lastName: "Doe",
-    email: "john@email.com"
-  })
+  const [displayFirstName, setDisplayFirstName] = useState("")
+  const [displayLastName, setDisplayLastName] = useState("")
+  const [displayEmail, setDisplayEmail] = useState("")
 
-  const handleChange = (key: any, value: any) => {
-      setForm((prev) => 
-      ({
-        ...prev,
-        [key]: value,
-      })
-    );
+  const onSubmit = () => {
+    setDisplayEmail(watch("email"));
+    setDisplayFirstName(watch("firstName"))
+    setDisplayLastName(watch("lastName"))
   }
+
+  useEffect(() =>{
+    setDisplayEmail(watch("email"));
+    setDisplayFirstName(watch("firstName"))
+    setDisplayLastName(watch("lastName"))
+  }, []);
 
   return (
     <View
@@ -36,11 +45,11 @@ const ProfileScreen = () => {
         </View>
 
         <Text className="mt-4 text-3xl font-semibold text-theme-text">
-          {form.firstName} {form.lastName}
+          {displayFirstName} {displayLastName}
         </Text>
 
         <Text className="text-2xl text-theme-icon">
-          {form.email}
+          {displayEmail}
         </Text>
       </View>
 
@@ -49,47 +58,119 @@ const ProfileScreen = () => {
       >
         <View>
           <Text className="text-2xl mb-1 text-theme-icon">
-            {form.firstName}
+            First Name 
           </Text>
 
-          <TextInput
-            editable={isEditing}
-            value={form.firstName}
-            className="p-4 text-xl border border-theme-text rounded-md bg-theme-background text-theme-text"
-            onChangeText={(text) => handleChange("firstName", text)}
+          <Controller
+            control={control}
+            name="firstName"
+            rules={{
+              required:"First Name is required",
+              maxLength: {
+                value: 20,
+                message: "First Name is too long",
+              },
+            }}
+            render={({ field: {onChange, value}}) => (
+              <TextInput
+                editable={isEditing}
+                value={value}
+                className="p-4 text-xl border border-theme-text rounded-md bg-theme-background text-theme-text"
+                onChangeText={onChange}
+              />
+            )}
           />
+
+          {errors.firstName && (
+            <Text className="text-red-500 text-sm pl-2 mt-1">
+                {errors.firstName.message}
+            </Text>
+          )}
+
         </View>
 
         <View>
           <Text className="text-2xl mb-1 text-theme-icon">
-            {form.lastName}
+            Last Name
           </Text>
 
-          <TextInput
-            editable={isEditing}
-            value={form.lastName}
-            className="p-4 text-xl border border-theme-text rounded-md bg-theme-background text-theme-text"
-            onChangeText={(text) => handleChange("lastName", text)}
+          <Controller
+            control={control}
+            name="lastName"
+            rules={{
+              required:"First Name is required",
+              maxLength: {
+                value: 20,
+                message: "First Name is too long",
+              },
+            }}
+            render={({ field: {onChange, value}}) => (
+              <TextInput
+                editable={isEditing}
+                value={value}
+                className="p-4 text-xl border border-theme-text rounded-md bg-theme-background text-theme-text"
+                onChangeText={onChange}
+              />
+            )}
           />
+
+          {errors.lastName && (
+            <Text className="text-red-500 text-sm pl-2 mt-1">
+                {errors.lastName.message}
+            </Text>
+          )}
         </View>
 
         <View>
           <Text className="text-2xl mb-1 text-theme-icon">
-            {form.email}
+            Email
           </Text>
 
-          <TextInput
-            editable={isEditing}
-            value={form.email}
-            className="p-4 text-xl border border-theme-text rounded-md bg-theme-background text-theme-text"
-            onChangeText={(text) => handleChange("email", text)}
+          <Controller
+            control={control}
+            name="email"
+            rules={{
+              required:"First Name is required",
+              maxLength: {
+                value: 20,
+                message: "First Name is too long",
+              },
+              pattern: {
+                value: /^\S+@\S+\.\S+$/,
+                message: "Invalid email format",
+              },
+            }}
+            render={({ field: {onChange, value}}) => (
+              <TextInput
+                editable={isEditing}
+                value={value}
+                className="p-4 text-xl border border-theme-text rounded-md bg-theme-background text-theme-text"
+                onChangeText={onChange}
+              />
+            )}
           />
+
+          {errors.email && (
+            <Text className="text-red-500 text-sm pl-2 mt-1">
+                {errors.email.message}
+            </Text>
+          )}
+          
         </View>
       </View>
 
       <TouchableOpacity
-        className={`mt-6 p-4 rounded-md items-center ${isEditing ? 'bg-theme-tint' : 'bg-theme-tint' }`}
-        onPress={() => setIsEditing((prev) => !prev)}
+        className={`mt-6 p-4 rounded-md items-center ${isEditing ? 'bg-theme-success' : 'bg-theme-tint' }`}
+        onPress={() => {
+            handleSubmit(() => {
+                setIsEditing(prev => !prev)
+                onSubmit()
+              },
+              (errors) => {
+                console.log("Validation failed", errors)
+              }
+            )();
+          }}
       >
         <Text 
         className="text-xl text-center text-theme-textLight font-semibold"
