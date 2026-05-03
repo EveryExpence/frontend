01b8d2d624e776823a27d33d69b4d22335164771
justkinou <justkinou@proton.me>
2026-05-03 12:57:08 +0200

    add generic modal component

---
+++ components/Modal.tsx
@@ -0,0 +1,37 @@
+import { View, Text, Modal, Pressable } from 'react-native'
+import React, { Dispatch, ReactNode, SetStateAction } from 'react'
+
+interface Props {
+    isVisible: boolean;
+    setIsVisible: Dispatch<SetStateAction<boolean>>;
+    title: string;
+    cancelAction: ReactNode | null;
+    confirmAction: ReactNode | null;
+    children: ReactNode;
+}
+
+const CustomModal = (props: Props) => {
+    const onClose = () => {
+        props.setIsVisible(false);
+    }
+
+    return (
+        <Modal visible={props.isVisible} transparent animationType="fade" onRequestClose={onClose}>
+            <Pressable onPress={onClose} className="flex-1 bg-black/35 flex items-center justify-center">
+                <Pressable onPress={() => { }} className="bg-theme-surface rounded-md w-11/12 p-6">
+                    <Text className="text-2xl font-semibold text-theme-text mb-4">{props.title}</Text>
+
+                    {props.children}
+
+                    <View className="flex-row justify-between items-center">
+                        {props.cancelAction ?? <></>}
+
+                        {props.confirmAction ?? <></>}
+                    </View>
+                </Pressable>
+            </Pressable>
+        </Modal>
+    )
+}
+
+export default CustomModal
--- components/new-expense/CategorySelection.tsx
+++ components/new-expense/CategorySelection.tsx
@@ -1,15 +1,20 @@
-import { View, Text, useColorScheme, TouchableOpacity } from 'react-native'
+import { View, Text, useColorScheme, TouchableOpacity, TextInput } from 'react-native'
 import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { Dropdown } from 'react-native-element-dropdown'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { Category } from '@/types/data/category'
 import { Colors } from '@/constants/theme'
-import { useSQLiteContext } from 'expo-sqlite'
+import { SQLiteDatabase, useSQLiteContext } from 'expo-sqlite'
 import { getAllCategories } from '@/data/categories'
+import CustomModal from '../Modal'
 
 interface Props {
     selectedCategory: Category | null;
     setSelectedCategory: Dispatch<SetStateAction<Category | null>>;
+}
+
+const fetchCategories = async (db: SQLiteDatabase, callback: Dispatch<SetStateAction<Category[]>>) => {
+    callback(await getAllCategories(db));
 }
 
 const CategorySelection = ({ selectedCategory, setSelectedCategory }: Props) => {
@@ -17,16 +22,16 @@ const CategorySelection = ({ selectedCategory, setSelectedCategory }: Props) => 
     const colors = Colors[scheme];
     const db = useSQLiteContext();
     const [categories, setCategories] = useState<Category[]>([]);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [newCategoryName, setNewCategoryName] = useState("");
 
     useEffect(() => {
-        (async () => {
-            setCategories(await getAllCategories(db));
-        })();
+        fetchCategories(db, setCategories);
     }, [db]);
 
     return (
         <View className="mb-8">
-            <Text className="text-2xl font-bold">Category</Text>
+            <Text className="text-2xl font-bold text-theme-text">Category</Text>
 
             <View className="flex-row justify-between items-center gap-2">
                 <Dropdown
@@ -38,12 +43,15 @@ const CategorySelection = ({ selectedCategory, setSelectedCategory }: Props) => 
                     }}
                     selectedTextStyle={{
                         fontSize: 18,
+                        color: colors.text,
                     }}
                     placeholderStyle={{
                         fontSize: 18,
+                        color: colors.text,
                     }}
                     inputSearchStyle={{
                         fontSize: 18,
+                        color: colors.text,
                     }}
                     data={categories}
                     search
@@ -59,22 +67,63 @@ const CategorySelection = ({ selectedCategory, setSelectedCategory }: Props) => 
                             className="mr-6"
                             name="chart-waterfall"
                             size={20}
+                            color={colors.text}
                         />
                     )}
                     renderRightIcon={() => (
                         <MaterialCommunityIcons
                             name="chevron-down"
                             size={20}
+                            color={colors.text}
                         />
                     )}
                 />
 
-                <TouchableOpacity>
-                    <MaterialCommunityIcons name="plus" size={32} />
+                <TouchableOpacity onPress={() => setIsModalVisible(true)}>
+                    <MaterialCommunityIcons name="plus" size={32} color={colors.text} />
                 </TouchableOpacity>
             </View>
-        </View>
 
+            <CustomModal
+                isVisible={isModalVisible}
+                setIsVisible={setIsModalVisible}
+                title="Create a new category"
+                cancelAction={
+                    <TouchableOpacity onPress={() => setIsModalVisible(false)}>
+                        <Text className="text-xl text-theme-text">Cancel</Text>
+                    </TouchableOpacity>
+                }
+                confirmAction={
+                    <TouchableOpacity
+                        onPress={() => { }}
+                        className="bg-theme-tint rounded-md px-5 py-3"
+                    >
+                        <Text className="text-xl text-theme-textLight">Save</Text>
+                    </TouchableOpacity>
+                }
+            >
+                <View className="w-full mb-4">
+                    <Text className="text-xl text-theme-text opacity-85">Category name</Text>
+
+                    <View className="w-full flex-row items-center">
+                        <MaterialCommunityIcons
+                            className="absolute pl-4 z-30"
+                            name="abugida-thai"
+                            size={20}
+                            color={colors.text}
+                        />
+
+                        <TextInput
+                            placeholder='Enter name'
+                            placeholderClassName="text-theme-text opacity-35"
+                            value={newCategoryName}
+                            onChangeText={setNewCategoryName}
+                            className="px-12 w-full py-4 text-xl rounded-md bg-theme-surface text-theme-text border border-theme-text"
+                        />
+                    </View>
+                </View>
+            </CustomModal>
+        </View>
     )
 }
 
