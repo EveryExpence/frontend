03cd08f0baee642be037cee3b27c96fb8519a992
Mikita Dzeviatau <9mikita@gmail.com>
2026-05-07 02:03:18 +0200

    Add gradient background on dashboard

--- app/(main)/dashboard.tsx
+++ app/(main)/dashboard.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { useAuth } from '@/context/authContext';
-import { TouchableOpacity, Text } from 'react-native';
-import Toast from 'react-native-toast-message';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React from "react";
+import { useAuth } from "@/context/authContext";
+import { TouchableOpacity, Text } from "react-native";
+import Toast from "react-native-toast-message";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { Colors } from "@/constants/theme";
 
 const Main = () => {
   const { user, logout } = useAuth();
+  const colorScheme: "light" | "dark" = useColorScheme() ?? "light";
+  const colors = Colors[colorScheme];
 
   const onPress = async () => {
     try {
@@ -16,18 +21,15 @@ const Main = () => {
   };
 
   return (
-    <SafeAreaView className="flex flex-1 justify-center items-center">
-      <Text className="2xl">
-        { user?.email } { user?.publicUsername }
-      </Text>
+    <LinearGradient
+      colors={[colors.surface, colors.tint]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      className="flex flex-1"
+    >
+      <SafeAreaView className="flex flex-1 justify-center items-center"></SafeAreaView>
+    </LinearGradient>
+  );
+};
 
-      <TouchableOpacity onPress={onPress}>
-        <Text className="2xl">
-          Logout
-        </Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  )
-}
-
-export default Main
+export default Main;
