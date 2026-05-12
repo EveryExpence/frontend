8d9b95e120f26449a48e927a0ec606ac3734c02f
Dmytro Malinovskyi <malynovskydmitriyy@gmail.com>
2026-05-12 18:04:48 +0200

    update useColroScheme hook

--- hooks/use-color-scheme.ts
+++ hooks/use-color-scheme.ts
@@ -1,1 +1,13 @@
-export { useColorScheme } from 'react-native';
+import { useContext } from "react";
+import { useColorScheme as useSystemColorScheme } from "react-native";
+import { ThemeContext } from "@/context/themeContext";
+
+export function useColorScheme() {
+	const context = useContext(ThemeContext);
+
+	if (context?.theme) {
+		return context.theme;
+	}
+
+	return useSystemColorScheme() ?? "light";
+}
--- hooks/use-color-scheme.web.ts
+++ hooks/use-color-scheme.web.ts
@@ -1,15 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useColorScheme as useRNColorScheme } from 'react-native';
+import { ThemeContext } from '@/context/themeContext';
 
 /**
  * To support static rendering, this value needs to be re-calculated on the client side for web
  */
 export function useColorScheme() {
   const [hasHydrated, setHasHydrated] = useState(false);
+  const context = useContext(ThemeContext);
 
   useEffect(() => {
     setHasHydrated(true);
   }, []);
+
+  if (context?.theme) {
+    return context.theme;
+  }
 
   const colorScheme = useRNColorScheme();
 
