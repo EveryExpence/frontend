fc89bda9c252954b370301a3bab86bf885abd217
Dmytro Malinovskyi <malynovskydmitriyy@gmail.com>
2026-05-12 18:03:56 +0200

    apply theme provider in app context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Slot } from 'expo-router'
 import './global.css'
 import { AuthProvider } from '@/context/authContext'
+import { ThemeProvider } from '@/context/themeContext'
 import CustomizedToast from '@/components/Toast'
 import { SQLiteProvider } from 'expo-sqlite';
 import { migrateDatabase } from '@/data/init'
@@ -12,22 +13,24 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 const RootLayout = () => {
   return (
     <SafeAreaProvider>
-      <View
-        style={{
-          flex: 1,
-        }}
-        className="bg-theme-background"
-      >
-        <AuthProvider>
-          <SQLiteProvider 
-            databaseName="app.db"
-            onInit={migrateDatabase}
-          >
-              <Slot />
-          </SQLiteProvider>
-        </AuthProvider>
-        <CustomizedToast />
-      </View>
+      <ThemeProvider>
+        <View
+          style={{
+            flex: 1,
+          }}
+          className="bg-theme-background"
+        >
+          <AuthProvider>
+            <SQLiteProvider 
+              databaseName="app.db"
+              onInit={migrateDatabase}
+            >
+                <Slot />
+            </SQLiteProvider>
+          </AuthProvider>
+          <CustomizedToast />
+        </View>
+      </ThemeProvider>
     </SafeAreaProvider>
   )
 }
