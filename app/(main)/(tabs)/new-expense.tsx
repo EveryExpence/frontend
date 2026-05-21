cbdfa6bc2769a47e50dacf638f3122b0227a45c9
justkinou <justkinou@proton.me>
2026-05-21 08:35:57 +0200

    fix crazy linter warnings 🙄🙄🙄

--- app/(main)/(tabs)/new-expense.tsx
+++ app/(main)/(tabs)/new-expense.tsx
@@ -69,7 +69,7 @@ export default function NewExpense() {
       setDescription("");
       setImages([]);
       setLocation(null);
-    } catch (e) {
+    } catch {
       Toast.show({ text1: "Failed to add a new record", type: "error" });
     }
   }
