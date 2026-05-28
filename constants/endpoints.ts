5d79f1775146cc5b5bfea1f4534c4b6ac50409ce
justkinou <justkinou@proton.me>
2026-05-28 02:24:26 +0200

    add payment method and expense record endpoints

--- constants/endpoints.ts
+++ constants/endpoints.ts
@@ -21,3 +21,13 @@ export const updateCategoryEndpoint = (id: string) => `${base}/categories/${id}`
 export const deleteCategoryEndpoint = (id: string) => `${base}/categories/${id}`;
 export const createCategoryEndpoint = `${base}/categories/create`;
 export const getCategoryEndpoint = `${base}/categories/getAll`;
+
+export const getPaymentMethodsEndpoint = `${base}/paymentMethod/getAll`;
+export const createPaymentMethodEndpoint = `${base}/paymentMethod/create`;
+export const updatePaymentMethodEndpoint = (id: string) => `${base}/paymentMethod/${id}`;
+export const deletePaymentMethodEndpoint = (id: string) => `${base}/paymentMethod/${id}`;
+
+export const getExpenseRecordsEndpoint = `${base}/expenseRecord/getAll`;
+export const createExpenseRecordEndpoint = `${base}/expenseRecord/create`;
+export const updateExpenseRecordEndpoint = (id: string) => `${base}/expenseRecord/${id}`;
+export const deleteExpenseRecordEndpoint = (id: string) => `${base}/expenseRecord/${id}`;
